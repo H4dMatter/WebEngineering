9622ff32bc3da8e700b3b82d44ea610d674f8374
Maximilian Claßen <maximilian.classen@hpe.com>
2019-08-02 12:47:21 +0200

    Add support for jQuery

--- js/events.js
+++ js/events.js
@@ -1,87 +1,95 @@
-var allEvents = [];
-var allCategories = [];
+$(document).ready(function () {
+    var allEvents = [];
+    var allCategories = [];
 
-/**
- * This function takes the inputs of the user and constructs a string(or maybe JSON object)
- * that can be fed to the addEvent function
- * @returns JSON object in event format
- * 
- */
-function assembleEventFromInput() {
-    var collection = document.getElementsByClassName("in");
-    console.log(collection);
-    for (let i = 0; i < collection.length; i++) {
-        console.log(collection[i].id + ":" + collection[i].innerHTML.trim());
-    }
-}
-
-//Test functions
-function test() {
-    var string = "";
-    for (let i = 0; i < allEvents.length; i++) {
-        console.log(allEvents[i]);
-        let element = allEvents[i];
-        for (var key in element) {
-            string += element[key] + " ";
+    /**
+     * This function takes the inputs of the user and constructs a string(or maybe JSON object)
+     * that can be fed to the addEvent function
+     * @returns JSON object in event format
+     * 
+     */
+    function assembleEventFromInput() {
+        var collection = document.getElementsByClassName("in");
+        console.log(collection);
+        for (let i = 0; i < collection.length; i++) {
+            console.log(collection[i].id + ":" + collection[i].innerHTML.trim());
         }
-        console.log(string);
-        string = "";
     }
-}
 
-//real functions
+    //Test functions
+    function test() {
 
-function addEvent(event) {
-    event = {
-        "id": 1, //ignoriert das id Feld automatisch -> auch andere Zusatzfelder(?)
-        "title": " Christmas Feast",
-        "location": "Stuttgart",
-        "organizer": "danny @dxc.com",
-        "start": "2014-12-24T18:00",
-        "end": "2014-12-24T23:00",
-        "status": "Busy",
-        "allday": false,
-        "webpage": "http://www.dxc.com/"
-    };
+        var string = "";
+        for (let i = 0; i < allEvents.length; i++) {
+            console.log(allEvents[i]);
+            let element = allEvents[i];
+            for (var key in element) {
+                string += element[key] + " ";
+            }
+            console.log(string);
+            string = "";
+        }
+    }
 
-    var xhttp = new XMLHttpRequest();
+    //real functions
+    $("#send").click(
+        function addEvent(event) {
+            event = {
+                "id": 1, //ignoriert das id Feld automatisch -> auch andere Zusatzfelder(?)
+                "title": " Christmas Feast",
+                "location": "Stuttgart",
+                "organizer": "danny @dxc.com",
+                "start": "2014-12-24T18:00",
+                "end": "2014-12-24T23:00",
+                "status": "Busy",
+                "allday": "false",
+                "webpage": "http://www.dxc.com/"
+            };
 
-    xhttp.open("POST", "https://dhbw.cheekbyte.de/calendar/claben/events", false);
-    xhttp.send(JSON.stringify(event));
+            $.ajax({
+                type: "POST",
+                url: "https://dhbw.cheekbyte.de/calendar/claben/events",
+                data: JSON.stringify(event),
+                success: function () { alert("success") },
+                error: function (xhr, status, error) { alert("fail " + xhr.status + xhr.statusText) }
+            });
+            // var xhttp = new XMLHttpRequest();
 
-    console.log("Fehler! " + error);
+            // xhttp.open("POST", "https://dhbw.cheekbyte.de/calendar/claben/events", false);
+            // xhttp.send(JSON.stringify(event));
 
-}
+        });
 
-function getAllEvents() {
-    allEvents = [];
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "https://dhbw.cheekbyte.de/calendar/claben/events", false);
-    xhttp.send();
+    function getAllEvents() {
+        allEvents = [];
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "https://dhbw.cheekbyte.de/calendar/claben/events", false);
+        xhttp.send();
 
-    let response = JSON.parse(xhttp.responseText);
-    allEvents = response;
-    console.log(response);
-}
+        let response = JSON.parse(xhttp.responseText);
+        allEvents = response;
+        console.log(response);
+    }
 
-function getCategories() {
-    allCategories = [];
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "https://dhbw.cheekbyte.de/calendar/claben/categories", false);
-    xhttp.send();
+    function getCategories() {
+        allCategories = [];
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "https://dhbw.cheekbyte.de/calendar/claben/categories", false);
+        xhttp.send();
 
-    let response = JSON.parse(xhttp.responseText);
-    allCategories = response;
-    console.log(response);
-}
+        let response = JSON.parse(xhttp.responseText);
+        allCategories = response;
+        console.log(response);
+    }
 
-function addCategory() {
-    allCategories = [];
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "https://dhbw.cheekbyte.de/calendar/claben/categories", false);
-    xhttp.send();
+    function addCategory() {
+        allCategories = [];
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "https://dhbw.cheekbyte.de/calendar/claben/categories", false);
+        xhttp.send();
 
-    let response = JSON.parse(xhttp.responseText);
-    allCategories = response;
-    console.log(response);
-}
+        let response = JSON.parse(xhttp.responseText);
+        allCategories = response;
+        console.log(response);
+    }
+});
